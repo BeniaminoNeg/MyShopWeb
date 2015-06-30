37af8806d7fd0b771b47b64eec20b6c6e71477d9
Montecchsilvia <montecchsilvia@gmail.com>
2015-06-30 11:35:44 +0200

    aggiunto campo prodottiosservati nella tabella utenteregistrato

--- Database/UtenteRegistrato.sql
+++ Database/UtenteRegistrato.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.3
+-- version 4.2.11
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Creato il: Giu 26, 2015 alle 19:38
--- Versione del server: 5.6.24
--- Versione PHP: 5.6.8
+-- Host: 127.0.0.1
+-- Generation Time: Giu 30, 2015 alle 11:31
+-- Versione del server: 5.6.21
+-- PHP Version: 5.6.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,41 +17,42 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `MyShopDB`
+-- Database: `myshopdb`
 --
 
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `UtenteRegistrato`
+-- Struttura della tabella `utenteregistrato`
 --
 
-CREATE TABLE IF NOT EXISTS `UtenteRegistrato` (
+CREATE TABLE IF NOT EXISTS `utenteregistrato` (
   `Password` varchar(20) COLLATE armscii8_bin NOT NULL,
   `Email` varchar(60) COLLATE armscii8_bin NOT NULL DEFAULT '',
   `Nome` varchar(20) COLLATE armscii8_bin NOT NULL,
-  `Cognome` varchar(20) COLLATE armscii8_bin NOT NULL
+  `Cognome` varchar(20) COLLATE armscii8_bin NOT NULL,
+  `prodottiosservati` varchar(1000) COLLATE armscii8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=armscii8 COLLATE=armscii8_bin;
 
 --
--- Dump dei dati per la tabella `UtenteRegistrato`
+-- Dump dei dati per la tabella `utenteregistrato`
 --
 
-INSERT INTO `UtenteRegistrato` (`Password`, `Email`, `Nome`, `Cognome`) VALUES
-('Coppitanno993', 'beniamino.negrni@gmail.com', 'Beniamino', 'Negrini'),
-('261293', 'gaetano.fichera93@gmail.com', 'Gaetano', 'Fichera'),
-('07031993', 'lordjhon16@gmail.com', 'Giovanni', 'Lezzi'),
-('070194', 'montecchsilvia@gmail.com', 'Silvia', 'Montecchia');
+INSERT INTO `utenteregistrato` (`Password`, `Email`, `Nome`, `Cognome`, `prodottiosservati`) VALUES
+('Coppitanno993', 'beniamino.negrni@gmail.com', 'Beniamino', 'Negrini', ''),
+('261293', 'gaetano.fichera93@gmail.com', 'Gaetano', 'Fichera', ''),
+('07031993', 'lordjhon16@gmail.com', 'Giovanni', 'Lezzi', ''),
+('070194', 'montecchsilvia@gmail.com', 'Silvia', 'Montecchia', '');
 
 --
--- Indici per le tabelle scaricate
+-- Indexes for dumped tables
 --
 
 --
--- Indici per le tabelle `UtenteRegistrato`
+-- Indexes for table `utenteregistrato`
 --
-ALTER TABLE `UtenteRegistrato`
-  ADD PRIMARY KEY (`Email`);
+ALTER TABLE `utenteregistrato`
+ ADD PRIMARY KEY (`Email`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
