5cb4daccc2032f6f6b9d61913713e826418461f4
BeniaminoNeg <beniamino.negrini@gmail.com>
2015-07-14 10:38:56 +0200

    personalizzati parametri sessione

--- Controller/CSessione.php
+++ Controller/CSessione.php
@@ -13,6 +13,10 @@ class CSessione {
     //put your code here
      public function Session(){
         session_start();
+        ini_set('session.name', "PHPSESSID_MYSHOP");
+        ini_set('session.cookie_lifetime', '1800');
+        ini_set('session.gc_maxlifetime', '1800');
+        ini_set('session.cookie_httponly', 'true');
         if (!isset($_SESSION['count']))
         {
             $_SESSION['count']=0;
